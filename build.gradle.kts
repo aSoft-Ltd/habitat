3252f7494a1df5f773bdb56b618bbd4449321189
andylamax <andylamax@programmer.net>
2025-05-17 21:53:45 +0300

    Migrated to Central Portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ subprojects {
     version = v
 
     configure<MavenPublishBaseExtension> {
-        publishToMavenCentral(SonatypeHost.DEFAULT,automaticRelease = true)
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL,automaticRelease = true)
 
         signAllPublications()
 
