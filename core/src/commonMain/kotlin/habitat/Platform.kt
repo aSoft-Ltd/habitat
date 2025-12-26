6fbdd7b6be61648b3744ca1f68dd70b16ebe249c
andylamax <andylamax@programmer.net>
2025-12-26 08:19:30 +0300

    Ensured that it does not have kollection or koncurrent

--- core/src/commonMain/kotlin/habitat/Platform.kt
+++ core/src/commonMain/kotlin/habitat/Platform.kt
@@ -1,6 +1,7 @@
 package habitat
 
 import habitat.runtime.Runtime
+import habitat.runtime.UnknownRuntime
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -11,4 +12,15 @@ sealed interface Platform {
     val device: Device
     val version: String
     val name get() = group.name
+
+    companion object {
+        val Unknown by lazy {
+            UnknownPlatform(
+                runtime = UnknownRuntime,
+                group = PlatformGroup.Unknown,
+                host = OperatingSystem.Unknown,
+                device = Device.Unknown
+            )
+        }
+    }
 }
--- core/src/jvmMain/kotlin/habitat/internal/PlatformManagerImpl.jvm.kt
+++ core/src/jvmMain/kotlin/habitat/internal/PlatformManagerImpl.jvm.kt
@@ -6,6 +6,8 @@ import habitat.OperatingSystem
 import habitat.Platform
 import habitat.PlatformManager
 import habitat.runtime.JvmRuntime
+import java.net.InetAddress
+
 
 @PublishedApi
 internal actual class PlatformManagerImpl actual constructor() : PlatformManager {
@@ -20,10 +22,17 @@ internal actual class PlatformManagerImpl actual constructor() : PlatformManager
         },
         runtime = JvmRuntime(version = System.getProperty("java.version")),
         device = Device(
-            name = "unknown",
-            model = "unknown",
+            name = getComputerName() ?: "Unknown",
+            model = "Unknown",
             cpu = System.getProperty("os.arch").toArch(),
             type = Device.Type.DESKTOP
         )
     )
+
+    //TODO: Find a way to test this on linux and windows as well
+    private fun getComputerName(): String? = try {
+        InetAddress.getLocalHost().getHostName()?.substringBefore(".")
+    } catch (_: Throwable) {
+        null
+    }
 }
